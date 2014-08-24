a4fc828f8e470a71cfee4358a909760c57f50f61
Paul Merrill <pdm@pdm.me>
2014-08-23 20:03:40 -0700

    fixed-backgrounds, animations, scroll-to-top on non-iOS

--- public/js/plugin_script.js
+++ public/js/plugin_script.js
@@ -13,7 +13,6 @@ $(window).load(function() {
     $('.faq').parallax("30%", .5);*/
 });
 
-
 //go to top button function
 $("#gotop").mouseenter(function(){
    $("#gotop").fadeTo("fast", 0.8);
@@ -23,6 +22,9 @@ $("#gotop").mouseleave(function(){
 });
    
 $(window).scroll(function() {
+  if (window.iOS) {
+    return;
+  }
   var windowHeight = window.innerHeight;
   if ($(this).scrollTop() > windowHeight){
     $('#gotop').fadeIn("fast");
@@ -39,3 +41,16 @@ if ($(this).scrollTop() > windowHeight) {
 $("#gotop").click(function(){
     $("html, body").animate({ scrollTop: 0}, 1000);
 });
+
+// iOS detection from http://stackoverflow.com/questions/9038625/detect-if-device-is-ios?lq=1
+window.iOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
+if (window.iOS) {
+    // The go-to-top button looks goofy on iOS with our current CSS.
+    $("#gotop").hide();
+
+    // Wow doesn't look activate while scrolling on iOS, which is confusing.  It activates after any scrolling completely stops.
+    $(".wow").removeClass("wow");
+    
+    // iOS doesn't support "background-attachment: fixed," and, in fact, does something weird, instead.
+    $(".fixed-background").removeClass("fixed-background");
+}
